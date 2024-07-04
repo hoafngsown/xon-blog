890f91cace746f3c4655cc915b4c04829e96cf6e
SonPhamHoang <sonph@kozo-japan.com>
2024-07-04 15:34:25 +0700

    chore: change env import

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,6 @@
-import envConfig from "@/configs/env";
-
 /** @type {import('next-sitemap').IConfig} */
 const nextSitemapConfig = {
-  siteUrl: envConfig.SITE_URL ?? "https://www.hoangson.space",
+  siteUrl: process.env.SITE_URL ?? "https://www.hoangson.space",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 };
