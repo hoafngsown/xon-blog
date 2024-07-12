22a4a4211b1effd27b447a3d502de733bf7159c4
SonPhamHoang <sonph@kozo-japan.com>
2024-07-12 18:03:54 +0700

    fix: build error

--- src/components/pages/blogs-detail/BlogView.tsx
+++ src/components/pages/blogs-detail/BlogView.tsx
@@ -14,22 +14,22 @@ export default function BlogView({ postId }: { postId: number }) {
     })();
   }, [postId]);
 
-  return view === null ? (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="animate-spin stroke-primary"
-    >
-      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
-    </svg>
-  ) : (
-    view
+  return (
+    view ?? (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="animate-spin stroke-primary"
+      >
+        <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+      </svg>
+    )
   );
 }
