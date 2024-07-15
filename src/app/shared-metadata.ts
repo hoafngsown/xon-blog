0a7be0ebcafdd59e5ba9b8c073aad697119b0a15
SonPhamHoang <sonph@kozo-japan.com>
2024-07-15 16:54:14 +0700

    chore: change logo

--- src/app/shared-metadata.ts
+++ src/app/shared-metadata.ts
@@ -1,9 +1,13 @@
 export const baseOpenGraph = {
-  locale: "en_US",
+  locale: ["vi_VN", "en_US"],
   type: "website",
   siteName: "Phạm Hoàng Sơn",
 };
 
 export const baseAlternates = {
   canonical: "/",
+  languages: {
+    "vi-VN": "/vi",
+    "en-US": "/en",
+  },
 };
