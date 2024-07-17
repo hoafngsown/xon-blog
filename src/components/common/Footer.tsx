e5a865f0a02c6f08f117f094fefc7d7eed37e5e3
SonPhamHoang <sonph@kozo-japan.com>
2024-07-17 10:39:08 +0700

    feat: change dark bg and footer text

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="mt-auto hidden border-t border-[#ddd]/70 bg-background shadow-sm md:block">
       <section className="flex items-center justify-center py-4 text-base text-black dark:text-title lg:text-lg">
-        <p className="font-medium text-primary">Hoàng Sơn</p>. © 2024 All Right
-        Reserved
+        <p className="font-medium text-primary">Hoàng Sơn</p>
+        <p className="ml-2 font-bold italic"> - Be where your feet are</p>
       </section>
     </footer>
   );
